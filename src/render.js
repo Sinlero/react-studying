fa400e89d352c57ba97342e8f674a35f88b1a178
goldfinch_p8a <wanted1345@gmail.com>
2021-08-05 13:20:21 +0900

    add props change texts and send message

--- src/render.js
+++ src/render.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {addPost} from "./redux/state";
+import {addPost, changeNewMessageText, changeNewPostText, sendMessage} from "./redux/state";
 
 export let rerenderEntireTree = (props) => {
     ReactDOM.render(
         <React.StrictMode>
-            <App state={props} addPost={addPost}/>
+            <App state={props}
+                 addPost={addPost}
+                 changeText={changeNewPostText}
+                 sendMessage={sendMessage}
+                 changeMsgText={changeNewMessageText}
+            />
         </React.StrictMode>,
         document.getElementById('root')
     );
