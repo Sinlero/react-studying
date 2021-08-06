a447e652757ab8836477e03aa6ba2b347ef8c14e
goldfinch_p8a <wanted1345@gmail.com>
2021-08-06 13:13:03 +0900

    move logic from deleted rerender.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
-import {rerenderEntireTree} from "./render";
-import state from "./redux/state";
+import state, {addPost, changeNewMessageText, changeNewPostText, sendMessage, subscribe} from "./redux/state";
+import ReactDOM from "react-dom";
+import React from "react";
+import App from "./App";
 
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App state={state}
+                 addPost={addPost}
+                 changeText={changeNewPostText}
+                 sendMessage={sendMessage}
+                 changeMsgText={changeNewMessageText}
+            />
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
+
