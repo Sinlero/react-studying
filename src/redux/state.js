a1cb4562fd8ceee291aaebdb97b8454faeaf9d66
Sinlero <wanted1345@gmail.com>
2021-08-07 20:35:36 +0900

    add dispatch method

--- src/redux/state.js
+++ src/redux/state.js
@@ -35,35 +35,45 @@ let store = {
     _callSubscriber() {
         console.log("state was changed");
     },
-    addPost() {
-        let newPost = {
-            id: 5,
-            message: this._state.profilePage.newPostText,
-            likes: 0
-        };
-        this._state.profilePage.postsData.push(newPost);
-        this._state.profilePage.newPostText = "";
-        this._callSubscriber(this._state);
-    },
-    changeNewPostText(newText) {
-        this._state.profilePage.newPostText = newText;
-        this._callSubscriber(this._state);
-    },
-    sendMessage() {
-        debugger;
-        let newMsg = {
-            id: 5,
-            message: this._state.dialogsPage.newMessageText
-        };
-        this._state.dialogsPage.messagesData.push(newMsg);
-    },
-    changeNewMessageText(newText) {
-        this._state.dialogsPage.newMessageText = newText;
-        this._callSubscriber(this._state);
-    },
     subscribe(observer) {
         this._callSubscriber = observer;
+    },
+
+    dispatch(action) {
+        switch (action.type) {
+            case "ADD-POST":
+                let newPost = {
+                    id: 5,
+                    name: "Admin",
+                    message: this._state.profilePage.newPostText,
+                    likes: 0
+                };
+                this._state.profilePage.postsData.push(newPost);
+                this._state.profilePage.newPostText = "";
+                this._callSubscriber(this._state);
+                break;
+            case "CHANGE-NEW-POST-TEXT":
+                this._state.profilePage.newPostText = action.newText;
+                this._callSubscriber(this._state);
+                break;
+            case "SEND-MESSAGE":
+                let newMsg = {
+                    id: 5,
+                    message: this._state.dialogsPage.newMessageText
+                };
+                this._state.dialogsPage.messagesData.push(newMsg);
+                this._state.dialogsPage.newMessageText = "";
+                this._callSubscriber(this._state);
+                break;
+            case "CHANGE-NEW-MESSAGE-TEXT":
+                this._state.dialogsPage.newMessageText = action.newText;
+                this._callSubscriber(this._state);
+                break;
+            default:
+                console.log("No such action");
+        }
     }
 };
 
 export default store;
+window.store = store;
