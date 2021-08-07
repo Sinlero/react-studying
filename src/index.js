1c827e2fcde8d121c64171173001965c68a4295c
Sinlero <wanted1345@gmail.com>
2021-08-07 18:43:08 +0900

    binding methods

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import state, {addPost, changeNewMessageText, changeNewPostText, sendMessage, subscribe} from "./redux/state";
+import store from "./redux/state";
 import ReactDOM from "react-dom";
 import React from "react";
 import App from "./App";
@@ -7,16 +7,16 @@ let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <React.StrictMode>
             <App state={state}
-                 addPost={addPost}
-                 changeText={changeNewPostText}
-                 sendMessage={sendMessage}
-                 changeMsgText={changeNewMessageText}
+                 addPost={store.addPost.bind(store)}
+                 changeText={store.changeNewPostText.bind(store)}
+                 sendMessage={store.sendMessage.bind(store)}
+                 changeMsgText={store.changeNewMessageText.bind(store)}
             />
         </React.StrictMode>,
         document.getElementById('root')
     );
 }
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
