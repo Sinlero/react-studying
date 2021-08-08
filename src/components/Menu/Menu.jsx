c6b4b8d2b6093ee1b883a4156459bed48a77de5c
Sinlero <wanted1345@gmail.com>
2021-08-08 11:25:29 +0900

    fix css style

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -5,19 +5,19 @@ import Friends from "./Friends/Friends";
 function Menu(props) {
     return(
         <nav className={style.nav}>
-            <div className={style.dialog}>
+            <div className={style.link}>
                 <NavLink to="/profile" activeClassName={style.active}>Profile</NavLink>
             </div>
-            <div className={style.dialog}>
+            <div className={style.link}>
                 <NavLink to="/dialogs" activeClassName={style.active}>Messages</NavLink>
             </div>
-            <div className={style.dialog}>
+            <div className={style.link}>
                 <NavLink to="/news" activeClassName={style.active}>News</NavLink>
             </div>
-            <div className={style.dialog}>
+            <div className={style.link}>
                 <NavLink to="/music" activeClassName={style.active}>Music</NavLink>
             </div>
-            <div className={style.dialog} >
+            <div className={style.link} >
                 <NavLink to="/settings" activeClassName={style.active}>Settings</NavLink>
             </div>
             <div>
