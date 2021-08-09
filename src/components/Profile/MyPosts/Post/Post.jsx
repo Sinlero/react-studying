4e19766f16df3a0a34a102a776359c6db1ff46e1
goldfinch_p8a <wanted1345@gmail.com>
2021-08-09 16:18:30 +0900

    use like work logic

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,6 +1,12 @@
 import style from "./Post.module.css";
+import {likePostActionCreator} from "../../../../redux/profileReducer";
 
 function Post(props) {
+
+    let addLike = () => {
+        props.dispatch(likePostActionCreator(props.id));
+    }
+
     return (
         <div className={style.post}>
             <img className={style.img}
@@ -16,7 +22,7 @@ function Post(props) {
                 <span>{props.likes} people likes this post</span>
             </div>
             <div className={style.like}>
-                <button>Like 👍</button>
+                <button onClick={addLike}>Like 👍</button>
             </div>
         </div>
     );
