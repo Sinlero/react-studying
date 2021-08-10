3a8aae251c5d2981e86ba9f5ac08dd1e8833e2eb
goldfinch_p8a <wanted1345@gmail.com>
2021-08-10 15:50:42 +0900

    add component with info about user

---
+++ src/components/Profile/ProfileInfo/AmoutMe/AboutMe.jsx
@@ -0,0 +1,19 @@
+import style from "./AboutMe.module.css"
+
+function AboutMe(props) {
+
+    let info = ["Birth place: Blaga Vegas", "Date birth: 22.02.2000", "Some bullshit info: kek"];
+    let render = info.map(el => <p>{el}</p>);
+
+
+    return (
+        <div className={style.block}>
+            <h3>About Me</h3>
+            <div>
+                {render}
+            </div>
+        </div>
+    )
+}
+
+export default AboutMe;
