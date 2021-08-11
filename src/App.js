a57c697d740b24fffebc26eb257d345b9c35e7dd
Sinlero <wanted1345@gmail.com>
2021-08-11 21:06:00 +0900

    only store

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Menu from "./components/Menu/Menu";
 import Profile from "./components/Profile/Profile";
-import Dialogs from "./components/Menu/Dialogs/Dialogs";
 import {BrowserRouter, Route} from "react-router-dom";
 import Settings from "./components/Menu/Settings/Settings";
 import Music from "./components/Menu/Music/Music";
 import News from "./components/Menu/News/News";
+import DialogsContainer from "./components/Menu/Dialogs/DialogsContainer";
 
 function App(props) {
     return (
@@ -16,11 +16,9 @@ function App(props) {
                 <Menu friends={props.state.dialogsPage.dialogsData}/>
                 <div className="app-wrapper-content">
                     <Route path="/dialogs"
-                           render={() => <Dialogs state={props.state.dialogsPage}
-                                                  dispatch={props.dispatch}/>}/>
+                           render={() => <DialogsContainer store={props.store}/>}/>
                     <Route path="/profile"
-                           render={() => <Profile
-                               store={props.store}/>}/>
+                           render={() => <Profile store={props.store}/>}/>
                     <Route path="/news" render={() => <News/>}/>
                     <Route path="/music" render={() => <Music/>}/>
                     <Route path="/settings" render={() => <Settings/>}/>
