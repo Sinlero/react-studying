918490e1373ab5091eedfdd74f2a52986b07bc70
Sinlero <wanted1345@gmail.com>
2021-08-13 10:59:19 +0900

    add design

--- src/components/Menu/Users/Users.jsx
+++ src/components/Menu/Users/Users.jsx
@@ -46,22 +46,23 @@ function Users(props) {
 
     return (
         <div>
-            {props.users.map(user => <div key={user.id}>
-                <span>
+            {props.users.map(user =>
+                <div className={style.usersItem} key={user.id}>
+
                     <div className={style.avatar}>
                         <img src={user.avatar} alt="ava"/>
                     </div>
+                    <div>Status: {user.status}</div>
+                    <div>{user.fullName}</div>
+                    <div>{user.location.city}</div>
                     <div>
                         {user.followed
                             ? <button onClick={() => props.unfollow(user.id)}>Unfollow</button>
                             : <button onClick={() => props.follow(user.id)}>Follow</button>}
                     </div>
-                </span>
-                <span>
-                    <span><div>{user.fullName}</div><div>{user.status}</div></span>
-                    <span><div>{user.location.city}</div><div>{user.location.country}</div></span>
-                </span>
-            </div>)}
+                    <div>{user.location.country}</div>
+
+                </div>)}
         </div>
     )
 }
