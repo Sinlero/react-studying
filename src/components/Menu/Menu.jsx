905bf9355404570c08003dbdc0e565712059a8f6
goldfinch_p8a <wanted1345@gmail.com>
2021-08-26 13:44:30 +0900

    change color of icons

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -15,37 +15,37 @@ function Menu(props) {
 
     return(
             <AntMenu mode="inline">
-                <AntMenu.Item key="Profile" icon={<HomeTwoTone />}>
+                <AntMenu.Item key="Profile" icon={<HomeTwoTone twoToneColor={"#4c00ff"} />}>
                     <div>
                         <NavLink to="/profile">Profile</NavLink>
                     </div>
                 </AntMenu.Item>
-                <AntMenu.Item key="Messages" icon={<MessageTwoTone />}>
+                <AntMenu.Item key="Messages" icon={<MessageTwoTone twoToneColor={"#4c00ff"} />}>
                     <div>
                         <NavLink to="/dialogs">Messages</NavLink>
                     </div>
                 </AntMenu.Item>
-                <AntMenu.Item key="News" icon={<FileTextTwoTone />}>
+                <AntMenu.Item key="News" icon={<FileTextTwoTone twoToneColor={"#4c00ff"} />}>
                     <div>
                         <NavLink to="/news">News</NavLink>
                     </div>
                 </AntMenu.Item>
-                <AntMenu.Item key="Music" icon={<CustomerServiceTwoTone />}>
+                <AntMenu.Item key="Music" icon={<CustomerServiceTwoTone twoToneColor={"#4c00ff"} />}>
                     <div>
                         <NavLink to="/music">Music</NavLink>
                     </div>
                 </AntMenu.Item>
-                <AntMenu.Item key="Settings" icon={<SettingTwoTone />}>
+                <AntMenu.Item key="Settings" icon={<SettingTwoTone twoToneColor={"#4c00ff"} />}>
                     <div>
                         <NavLink to="/settings">Settings</NavLink>
                     </div>
                 </AntMenu.Item>
-                <AntMenu.Item key="Users" icon={<SecurityScanTwoTone />}>
+                <AntMenu.Item key="Users" icon={<SecurityScanTwoTone twoToneColor={"#4c00ff"} />}>
                     <div>
                         <NavLink to="/users">Find Users</NavLink>
                     </div>
                 </AntMenu.Item>
-                <AntMenu.SubMenu key="Friends" title={"Friends"} icon={<SmileTwoTone />}>
+                <AntMenu.SubMenu key="Friends" title={"Friends"} icon={<SmileTwoTone twoToneColor={"#4c00ff"} />}>
                         <FriendsContainer/>
                 </AntMenu.SubMenu>
             </AntMenu>
