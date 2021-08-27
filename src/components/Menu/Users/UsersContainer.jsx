f619a21f06c12d9ef11096cb90366509dd4f9bd0
goldfinch_p8a <wanted1345@gmail.com>
2021-08-27 13:31:00 +0900

    add class component

--- src/components/Menu/Users/UsersContainer.jsx
+++ src/components/Menu/Users/UsersContainer.jsx
@@ -1,6 +1,33 @@
 import {connect} from "react-redux";
 import {followAC, setCurrentPageAC, setTotalUsersCountsAC, setUsersAC, unfollowAC} from "../../../redux/usersReducer";
-import UserClass from "./UserClass";
+import React from "react";
+import axios from "axios";
+import Users from "./Users";
+
+class UsersContainer extends React.Component {
+
+    //192.168.202.104:8081 | localhost:8081
+    componentDidMount() {
+        axios.get(`http://localhost:8081/users?page=${this.props.currentPage}&limit=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.users);
+            this.props.setTotalUsersCounts(response.data.totalRecords);
+        });
+    }
+
+    onPageClick = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`http://localhost:8081/users?page=${pageNumber}&limit=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.users);
+        });
+    }
+
+
+    render() {
+        return <Users totalRecords={this.props.totalRecords} pageSize={this.props.pageSize}
+                      currentPage={this.props.currentPage} onPageClick={this.onPageClick}
+                      users={this.props.users} follow={this.props.follow} unfollow={this.props.unfollow}/>
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
@@ -31,6 +58,4 @@ let mapDispatchToProps = (dispatch) => {
     }
 }
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UserClass)
-
-export default UsersContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer)
