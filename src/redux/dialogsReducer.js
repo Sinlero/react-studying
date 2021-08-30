16a13059ec30eeed7e25c6fb7f565a1e2de8dc5f
goldfinch_p8a <wanted1345@gmail.com>
2021-08-30 16:00:55 +0900

    add messages to state

--- src/redux/dialogsReducer.js
+++ src/redux/dialogsReducer.js
@@ -18,6 +18,22 @@ let initialState = {
         {id: 2, message: "of"},
         {id: 3, message: "JavaScript"},
         {id: 4, message: "Objects"},
+        {id: 5, message: "Objects"},
+        {id: 6, message: "Objects"},
+        {id: 7, message: "Objects"},
+        {id: 8, message: "Objects"},
+        {id: 9, message: "Objects"},
+        {id: 10, message: "Objects"},
+        {id: 11, message: "Objects"},
+        {id: 12, message: "Objects"},
+        {id: 13, message: "Objects"},
+        {id: 14, message: "Objects"},
+        {id: 15, message: "Objects"},
+        {id: 16, message: "Objects"},
+        {id: 17, message: "Objects"},
+        {id: 18, message: "Objects"},
+        {id: 19, message: "Objects"},
+        {id: 20, message: "Objects"}
     ],
     newMessageText: ""
 }
