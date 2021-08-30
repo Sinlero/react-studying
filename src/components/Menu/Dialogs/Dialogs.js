c0bbc65e88a3a4fe1e51df8a7e243e46738e9367
goldfinch_p8a <wanted1345@gmail.com>
2021-08-30 16:01:16 +0900

    add layouts

--- src/components/Menu/Dialogs/Dialogs.js
+++ src/components/Menu/Dialogs/Dialogs.js
@@ -1,18 +1,34 @@
 import style from "./Dialogs.module.css"
 import MessageBox from "./MessageBox/MessageBox";
+import {Layout} from 'antd';
 
 function Dialogs(props) {
 
     return (
         <div className={style.dialogs}>
-            <div className={style.dialogsItems}>
-                {props.dialogsElements}
-            </div>
-            <div className={style.messages}>
-                {props.messagesElements}
-                <MessageBox sendMessage={props.sendMessage} changeMessageText={props.changeMessageText}
-                            defaultText={props.newMessageText}/>
-            </div>
+
+            <Layout>
+                <Layout.Sider>
+                    <div className={style.dialogsItems}>
+                        {props.dialogsElements}
+                    </div>
+                </Layout.Sider>
+                <Layout>
+                    <Layout.Content>
+                        <div className={style.messages}>
+                            {props.messagesElements}
+                        </div>
+                    </Layout.Content>
+                    <Layout.Footer>
+                        <div className={style.messageBoxLayout}>
+                            <MessageBox sendMessage={props.sendMessage} changeMessageText={props.changeMessageText}
+                                        defaultText={props.newMessageText}/>
+                        </div>
+                    </Layout.Footer>
+                </Layout>
+
+            </Layout>
+
         </div>
     );
 }
