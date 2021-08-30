f474ae275e3aaeba5b0e52dcba63da821f55f635
goldfinch_p8a <wanted1345@gmail.com>
2021-08-30 16:01:29 +0900

    add react logo to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import style from "./Header.module.css";
+import logo from "../../avatars/reactLogo.png";
 
 function Header() {
     return (
         <header className={style.header}>
             <img
-                src="logo192.png"
+                src={logo}
                 alt="logo"/>
         </header>
     );
