afb1cdd25379cc877ff43187b9bf67cea88d1d68
goldfinch_p8a <wanted1345@gmail.com>
2021-09-05 16:13:30 +0900

    add navlink to avatar

--- src/components/Menu/Users/Users.jsx
+++ src/components/Menu/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import style from "./User.module.css";
 import reactLogo from "../../../avatars/reactLogo.png"
 import {Avatar, Button, Descriptions, Pagination} from "antd";
+import {NavLink} from "react-router-dom";
 
 function Users(props) {
 
@@ -9,26 +10,32 @@ function Users(props) {
         <div>
             <div className={style.pagination}>
                 <Pagination current={props.currentPage} pageSize={props.pageSize} total={props.totalRecords}
-                            onChange={props.onPageClick} onShowSizeChange={props.onChangePageSize} showSizeChanger={true} pageSizeOptions={[2,3,4]}/>
+                            onChange={props.onPageClick} onShowSizeChange={props.onChangePageSize}
+                            showSizeChanger={true} pageSizeOptions={[2, 3, 4]}/>
             </div>
 
             {props.users.map(user =>
                 <div className={style.userItem} key={user.id}>
-                    <Avatar size={64} src={user.photos !== null ? user.photos : reactLogo} alt="avatar" />
+                    <NavLink to={"/profile/" + user.id}>
+                        <Avatar size={64} src={user.photos !== null ? user.photos : reactLogo}
+                                     alt="avatar"/>
+                    </NavLink>
                     <Descriptions title="User Info">
                         <Descriptions.Item label="User Name" span={3}>{user.name}</Descriptions.Item>
                         <Descriptions.Item label="Status" span={3}>{user.status}</Descriptions.Item>
-                        <Descriptions.Item label="Live" >{"user.location.city"}</Descriptions.Item>
-                        <Descriptions.Item label="Address" >{"user.location.address"}</Descriptions.Item>
-                        <Descriptions.Item label="Country" >{"user.location.country"}</Descriptions.Item>
+                        <Descriptions.Item label="Live">{"user.location.city"}</Descriptions.Item>
+                        <Descriptions.Item label="Address">{"user.location.address"}</Descriptions.Item>
+                        <Descriptions.Item label="Country">{"user.location.country"}</Descriptions.Item>
                     </Descriptions>
                     {user.followed
-                        ? <Button type="primary"  onClick={() => props.unfollow(user.id)} value="Unfollow">Unfollow</Button>
+                        ? <Button type="primary" onClick={() => props.unfollow(user.id)}
+                                  value="Unfollow">Unfollow</Button>
                         : <Button type="primary" onClick={() => props.follow(user.id)} value="Follow">Follow</Button>}
                 </div>)}
             <div className={style.pagination}>
                 <Pagination current={props.currentPage} pageSize={props.pageSize} total={props.totalRecords}
-                            onChange={props.onPageClick} onShowSizeChange={props.onChangePageSize} showSizeChanger={true} pageSizeOptions={[2,3,4]}/>
+                            onChange={props.onPageClick} onShowSizeChange={props.onChangePageSize}
+                            showSizeChanger={true} pageSizeOptions={[2, 3, 4]}/>
             </div>
         </div>)
 }
