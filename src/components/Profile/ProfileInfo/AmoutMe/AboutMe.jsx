ab3f0286d9d7eafd220240e5eafdeb59564582f4
Sinlero <wanted1345@gmail.com>
2021-09-05 17:55:09 +0900

    render profile info

--- src/components/Profile/ProfileInfo/AmoutMe/AboutMe.jsx
+++ src/components/Profile/ProfileInfo/AmoutMe/AboutMe.jsx
@@ -2,15 +2,27 @@ import style from "./AboutMe.module.css"
 
 function AboutMe(props) {
 
-    let info = ["Birth place: Blaga Vegas", "Date birth: 22.02.2000", "Some bullshit info: kek"];
-    let render = info.map(el => <p key={el.charAt(0)}>{el}</p>);
-
+    let contactsArray = [
+        "Facebook: " + props.profile.contacts.facebook,
+        "Github: " + props.profile.contacts.github,
+        "Instagram: " + props.profile.contacts.instagram,
+        "Twitter: " + props.profile.contacts.twitter,
+        "Vk: " + props.profile.contacts.vk,
+        "Youtube: " + props.profile.contacts.youtube,
+        "Website: " + props.profile.contacts.website,
+        "Main Link: " + props.profile.contacts.mainLink];
+    let contacts = contactsArray.map(link => <p>{link}</p>)
 
     return (
         <div className={style.block}>
             <h3>About Me</h3>
             <div>
-                {render}
+                <p>{props.profile.name}</p>
+                <p>{props.profile.fullName}</p>
+                <p>{props.profile.aboutMe}</p>
+                {contacts}
+                <p>{props.profile.lookingForAJob}</p>
+                <p>{props.profile.lookingForAJobDescription}</p>
             </div>
         </div>
     )
